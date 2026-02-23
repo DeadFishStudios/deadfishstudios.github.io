b4c506f40db9ef7f9617cb6089123744473cec32
DeadFishStudios <afrcanpolarbear@protonmail.com>
2026-02-23 12:17:36 +0200

    mrt 6

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+const question = document.querySelector("#question");
+const q = [
+    "it is okay for two men to have the bum sex?",
+    "unrepentant gaysexualist can get blessed in a catholic church?",
+    "they can get sacramentally married?",
+    "I still go to hell for eating meat on a friday?",
+    "anybody off the street can handle consecrated hosts?",
+    "it is now Protestant?",
+    "I can divorce my wife and still go to communion?",
+    "the Dogmas of the Church are no longer Dogmatic?",
+    "if it is now synodal, why do I still have to listen to you?"
+]
+question.textContent = q[5]
