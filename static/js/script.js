18987a3141682b10b963e62075e2c4bf05db9df0
DeadFishStudios <afrcanpolarbear@protonmail.com>
2026-02-23 13:01:30 +0200

    mr t 7

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,17 @@
+const meme = document.querySelector(".meme");
 const question = document.querySelector("#question");
 const q = [
     "it is okay for two men to have the bum sex?",
     "unrepentant gaysexualist can get blessed in a catholic church?",
-    "they can get sacramentally married?",
+    "unrepentant gaysexualist can get sacramentally married?",
     "I still go to hell for eating meat on a friday?",
     "anybody off the street can handle consecrated hosts?",
     "it is now Protestant?",
     "I can divorce my wife and still go to communion?",
     "the Dogmas of the Church are no longer Dogmatic?",
-    "if it is now synodal, why do I still have to listen to you?"
+    "if it is now synodal, then why do I still have to listen to you?"
 ]
-question.textContent = q[5]
+question.textContent = q[0]
+meme.addEventListener('click', ()=> {
+    question.textContent = q[Math.floor(Math.random() * q.length)]
+})
