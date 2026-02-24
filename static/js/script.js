a166621f4d94d32d67de5c7d46472df2b711d440
DeadFishStudios <afrcanpolarbear@protonmail.com>
2026-02-24 14:09:13 +0200

    Mr tT xx 2

--- static/js/script.js
+++ static/js/script.js
@@ -1,18 +1,25 @@
 const meme = document.querySelector(".meme");
 const question = document.querySelector("#question");
 const q = [
-    "it is okay for two men to have the bum sex",
-    "unrepentant gaysexualist can get blessed in a Catholic Church",
-    "unrepentant gaysexualist can get sacramentally married in the Church",
-    "I still go to hell for eating meat on a Friday",
-    "anybody off the street can handle consecrated hosts",
     "it is Protestant",
+    "it is okay for two men to have the bum sex",
+    "unrepentant gaysexualists can get blessed in a Catholic Church",
+    "unrepentant gaysexualists can get sacramentally married in the Church",
+    "I still go to hell for eating meat on a Friday",
+    "anybody off the street can handle Consecrated Hosts",
     "I can divorce my wife and still go to communion",
     "the Dogmas of the Church are no longer Dogmatic",
-    // "if it is synodal, the why do I still have to listen to you",
+    "it is synodal, what is the point of Papal authority",
     "I can wrap my Jimmy in a jacket, and it's not a sin",
 ]
-question.textContent = "----"
+let x = 0;
+question.textContent = q[x]
 meme.addEventListener('click', ()=> {
-    question.textContent = q[Math.floor(Math.random() * q.length)]
+    if (x > q.length) {
+        x = 0;
+    } else {
+        x = x + 1;
+    }
+    // question.textContent = q[Math.floor(Math.random() * q.length)]
+    question.textContent = q[x]
 })
